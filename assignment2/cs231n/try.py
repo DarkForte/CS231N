85a1e4d2bc32812c9e608c07d62633c92c7e4f04
DarkZero <laserae86@126.com>
2017-02-16 20:09:08 +0800

    Passed the gradient check in assignment 2

--- assignment2/cs231n/try.py
+++ assignment2/cs231n/try.py
@@ -10,20 +10,20 @@ num_inputs = 2
 input_dim = (3, 16, 16)
 reg = 0.0
 num_classes = 10
+np.random.seed(2333)
 X = np.random.randn(num_inputs, *input_dim)
 y = np.random.randint(num_classes, size=num_inputs)
 
-model = ConvNet(num_filters=[], filter_size=[],
+model = ConvNet(num_filters=[3], filter_size=[3],
                 input_dim=input_dim, hidden_dim=[4, 4],
-                dtype=np.float64)
+                dtype=np.float64, weight_scale=1)
 loss, grads = model.loss(X, y)
 for param_name in sorted(grads):
   f = lambda _: model.loss(X, y)[0]
   param_grad_num = eval_numerical_gradient(f, model.params[param_name], verbose=False, h=1e-6)
 
-
-  print param_grad_num
-  print grads[param_name]
+  #print param_grad_num
+  #print grads[param_name]
 
   e = rel_error(param_grad_num, grads[param_name])
   print '%s max relative error: %e' % (param_name, rel_error(param_grad_num, grads[param_name]))
